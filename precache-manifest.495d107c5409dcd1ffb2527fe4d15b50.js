1925a9bb00e0032e6ccf72befeb9209a8085fe3b
Asish Shaji Thomas <mrkai1253@gmail.com>
2019-01-10 17:13:35 +0530

    Updates

---
+++ precache-manifest.495d107c5409dcd1ffb2527fe4d15b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4887231ecd08b1f26942",
+    "url": "/nivata/static/js/runtime~main.4887231e.js"
+  },
+  {
+    "revision": "dc992ab9f6a87915a070",
+    "url": "/nivata/static/js/main.dc992ab9.chunk.js"
+  },
+  {
+    "revision": "b6fe12764b730650e549",
+    "url": "/nivata/static/js/1.b6fe1276.chunk.js"
+  },
+  {
+    "revision": "dc992ab9f6a87915a070",
+    "url": "/nivata/static/css/main.49a8bb5b.chunk.css"
+  },
+  {
+    "revision": "b6fe12764b730650e549",
+    "url": "/nivata/static/css/1.d0cb113b.chunk.css"
+  },
+  {
+    "revision": "78edee6f8cc0354723de84ab870ce9ad",
+    "url": "/nivata/index.html"
+  }
+];
